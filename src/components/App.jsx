0bc742e4eebc5c4bb4393711daa931bbfa697dbd
Kreal11 <kirillshyrokov2@gmail.com>
2023-10-20 21:39:46 +0300

    refractored code step 3 task 1 hw-2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import { Component } from 'react';
 import { StyledMainWrapper } from './Statistics/FeedbackStat.styled';
 import { Statistics } from './Statistics/Statistics';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Section } from './Section/Section';
 
 export class App extends Component {
   state = {
@@ -34,19 +35,21 @@ export class App extends Component {
 
     return (
       <StyledMainWrapper>
-        <h1>PLEASE LEAVE FEEDBACK</h1>
-        <FeedbackOptions
-          options={Object.keys(this.state)}
-          onLeaveFeedback={this.handleChangeStat}
-        ></FeedbackOptions>
-        <h2>Statistics</h2>
-        <Statistics
-          good={this.state.good}
-          neutral={this.state.neutral}
-          bad={this.state.bad}
-          total={totalFeedback}
-          positivePercentage={totalPositiveFeedbacks}
-        ></Statistics>
+        <Section title="PLEASE, LEAVE YOUR FEEDBACK">
+          <FeedbackOptions
+            options={Object.keys(this.state)}
+            onLeaveFeedback={this.handleChangeStat}
+          />
+        </Section>
+        <Section title="Statistics">
+          <Statistics
+            good={this.state.good}
+            neutral={this.state.neutral}
+            bad={this.state.bad}
+            total={totalFeedback}
+            positivePercentage={totalPositiveFeedbacks}
+          />
+        </Section>
       </StyledMainWrapper>
     );
   }
