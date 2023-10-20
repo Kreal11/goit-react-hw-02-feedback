894523d05f764fc10808a56db1b448a479b2e7c7
Kreal11 <kirillshyrokov2@gmail.com>
2023-10-20 18:50:30 +0300

    done 50% of 1st task from hw 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,56 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { StyledMainWrapper } from './FeedbackStat/StyledFeedbackStat.styled';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleChangeStat = name => {
+    this.setState(prevState => {
+      return { [name]: prevState[name] + 1 };
+    });
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    const total = this.countTotalFeedback();
+    return total > 0 ? (good / total) * 100 : 0;
+  };
+
+  render() {
+    const totalFeedback = this.countTotalFeedback();
+    const totalPositiveFeedbacks =
+      this.countPositiveFeedbackPercentage().toFixed();
+
+    return (
+      <StyledMainWrapper>
+        <h1>PLEASE LEAVE FEEDBACK</h1>
+        <button type="button" onClick={() => this.handleChangeStat('good')}>
+          Good
+        </button>
+        <button type="button" onClick={() => this.handleChangeStat('neutral')}>
+          Neutral
+        </button>
+        <button type="button" onClick={() => this.handleChangeStat('bad')}>
+          Bad
+        </button>
+        <h2>Statistics</h2>
+        <ul>
+          <li>Good: {this.state.good}</li>
+          <li>Neutral: {this.state.neutral}</li>
+          <li>Bad: {this.state.bad}</li>
+        </ul>
+        <p>Total: {totalFeedback}</p>
+        <p>Positive feedbacks: {totalPositiveFeedbacks}%</p>
+      </StyledMainWrapper>
+    );
+  }
+}
---
+++ src/components/FeedbackStat/StyledFeedbackStat.styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const StyledMainWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 300px;
+  margin: 0 auto;
+  text-align: center;
+`;
