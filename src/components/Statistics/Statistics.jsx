0d1da7f2ab51e36cce1306db2d0f54117d80e824
Kreal11 <kirillshyrokov2@gmail.com>
2023-10-20 20:29:43 +0300

    add component statistics as a class

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,17 +1,16 @@
-export const Statistics = ({
-  good,
-  neutral,
-  bad,
-  total,
-  positivePercentage,
-}) => {
-  return (
-    <ul>
-      <li>Good: {good}</li>
-      <li>Neutral: {neutral}</li>
-      <li>Bad: {bad}</li>
-      <li>Total: {total}</li>
-      <li>Positive feedbacks: {positivePercentage}%</li>
-    </ul>
-  );
-};
+import { Component } from 'react';
+
+export class Statistics extends Component {
+  render() {
+    const { good, neutral, bad, total, positivePercentage } = this.props;
+    return (
+      <ul>
+        <li>Good: {good}</li>
+        <li>Neutral: {neutral}</li>
+        <li>Bad: {bad}</li>
+        <li>Total: {total}</li>
+        <li>Positive feedbacks: {positivePercentage}%</li>
+      </ul>
+    );
+  }
+}
